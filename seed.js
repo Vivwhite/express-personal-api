231e403175122d0cf48f35252282af8503ff2c22
vivienne white <vivihansf@gmail.com>
2017-05-19 14:34:04 -0500

    added one movie

--- seed.js
+++ seed.js
@@ -1,10 +1,22 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+   var db = require('./models');
 
 // var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+var newMovie = {
+  title: 'Pulp Fiction',
+  genres: ' neo-noir',
+  director: 'Quentin Tarantino',
+  releaseDate: 10141994
+}
 
+db.Movie.create(newMovie, function(err, movie){
+  if (err){
+    return console.log("Error:", err);
+  }
+  process.exit();
+})
 // db.Campsite.create(new_campsite, function(err, campsite){
 //   if (err){
 //     return console.log("Error:", err);
